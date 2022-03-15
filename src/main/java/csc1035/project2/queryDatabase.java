899bc00a486b998b9aa84121b5147ff7e7f80826
c1018275 <c1018275@newcastle.ac.uk>
2022-03-15 13:04:53 +0000

    Outlined the queryDatabase methods and class, will implement these methods at a later date

---
+++ src/main/java/csc1035/project2/queryDatabase.java
@@ -0,0 +1,18 @@
+package csc1035.project2;
+
+/**
+ * Created a class which will be used to query the database
+ */
+public class queryDatabase {
+    /**
+     * Defined a constructor
+     */
+    public static void queryDatabase(){
+    }
+    public ArrayList<Question> searchQuestions(){
+    }
+    public ArrayList<Question> viewIncorrectQuestions(){
+    }
+    public ArrayList<Question> randomQuestions(int size){
+    }
+}
