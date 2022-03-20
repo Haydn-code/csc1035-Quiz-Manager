896c75968e0c164c85e02cb234e328dd051a8f9e
Haydn Small <91317802+Haydn-code@users.noreply.github.com>
2022-03-20 17:26:23 +0000

    Started to implement the searchQuestions method

--- src/main/java/csc1035/project2/queryDatabase.java
+++ src/main/java/csc1035/project2/queryDatabase.java
@@ -1,5 +1,9 @@
 package csc1035.project2;
 
+import java.util.Scanner;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
 /**
  * Created a class which will be used to query the database
  */
@@ -7,12 +11,41 @@ public class queryDatabase {
     /**
      * Defined a constructor
      */
-    public static void queryDatabase(){
+    public queryDatabase(){
     }
-    public ArrayList<Question> searchQuestions(){
+    public static ArrayList<Question> returnAllQuestions(){
+        Session
     }
-    public ArrayList<Question> viewIncorrectQuestions(){
+    public static ArrayList<Question> searchQuestions(){
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a number between 1-3");
+        System.out.println("1. Search by question ID");
+        System.out.println("2. Search by type");
+        System.out.println("3. Search by topic");
+        while (!sc.hasNextInt()) { //checks the input is an integer
+            System.out.println("Please enter an int 1-13");
+            sc.nextLine();
+        }
+        int selection = sc.nextInt();
+        sc.nextLine();
+        switch(selection){
+            case 1:
+                while (true) {
+                    s.beginTransaction();
+                    System.out.println("Please enter the question ID you would like to search for?");
+                    while (!sc.hasNextInt()) {
+                        System.out.println("Please enter and integer");
+                        sc.nextLine();
+                    }
+                    int questionID = sc.nextInt();
+                    String query = "from Questions q where q.questionID = " + questionID;
+                    Query searchQuestionID = s.createQuery(query);
+            }
+        }
     }
-    public ArrayList<Question> randomQuestions(int size){
+    public static ArrayList<Question> viewIncorrectQuestions(){
+    }
+    public static ArrayList<Question> randomQuestions(int size){
     }
 }
