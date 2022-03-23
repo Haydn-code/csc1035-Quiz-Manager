fa35b16e8ba4919251867e24dc5ca09a0bb140e9
Haydn Small <91317802+Haydn-code@users.noreply.github.com>
2022-03-23 22:37:13 +0000

    Added and implemented the updateQuiz and deleteQuiz methods to the class

--- src/main/java/csc1035/project2/CrudQuiz.java
+++ src/main/java/csc1035/project2/CrudQuiz.java
@@ -112,17 +112,7 @@ public class CrudQuiz {
             sc.nextLine();
             switch(choice) {
                 case 1:
-                    System.out.println("Please enter the questionID of the question you wish to add to the quiz");
-                    while (!sc.hasNextInt()) {
-                        System.out.println("The questionID must be an integer");
-                        sc.nextLine();
-                    }
-                    int questionID = sc.nextInt();
-                    Query query = s.createQuery("from questions q where q.questionID = " + questionID);
-                    Question q = null;
-                    for (Object i : query.getResultList()) {
-                        q = (Question) i;
-                    }
+                    Question q = CrudQuestions.selectQuestion(s, sc);
                     QuizQuestions qQ = new QuizQuestions(quiz, q);
                     s.save(qQ);
                     quizQuestions.add(qQ);
@@ -150,6 +140,7 @@ public class CrudQuiz {
      */
     public static void readQuiz(Session s){
         s.beginTransaction();
+        //iterates through every quizes entity and outputs it to the console
         Query q = s.createQuery("from quizes");
         for (Object i : q.getResultList()){
             Quiz quiz = (Quiz) i;
@@ -157,17 +148,83 @@ public class CrudQuiz {
         }
     }
 
+    /**
+     * Defined a method which allows the user to update an existing quiz
+     * @param s - the hibernate session that is open for the duration of the program
+     */
     public static void updateQuiz(Session s){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter the quizID of the quiz you would wish to update");
-        while(!sc.hasNextInt()){
-            System.out.println("The quizID must be an integer");
+        s.beginTransaction();
+        Quiz quiz = selectQuiz(s, sc); //allows the user to select which quiz they wish to update
+        boolean loop = true;
+        while (loop) { //code will loop until the user decides they have finished updating the quiz
+            System.out.println(quiz);
+            System.out.println("Please enter a integer 1-3");
+            System.out.println("1. Add a question to the quiz");
+            System.out.println("2. Remove a question from the quiz");
+            System.out.println("3. Finish and save quiz to the database");
+            while (!sc.hasNextInt()) {//ensures that the user enters an integer
+                System.out.println("Please enter an integer 1-3");
+                sc.nextLine();
+            }
+            int choice = sc.nextInt();
+            //runs the corresponding code to the user input
+            switch (choice) {
+                case 1:
+                    Question q = CrudQuestions.selectQuestion(s, sc);//allows the user to select which question they
+                    //wish to add to the quiz
+                    QuizQuestions qQ = new QuizQuestions(quiz, q);
+                    s.save(qQ);
+                    List<QuizQuestions> qQuestions = quiz.getqQuestions();
+                    qQuestions.add(qQ);
+                    break;
+                case 2:
+                    //allows the user to select the relationship between Quiz and QuizQuestion that they wish to remove
+                    System.out.println("Please enter the QQJD of the QuizQuestion you wish to remove");
+                    while (!sc.hasNextInt()) {
+                        System.out.println("Please enter an integer");
+                        sc.nextLine();
+                    }
+                    int QQJD = sc.nextInt();
+                    sc.nextLine();
+                    QuizQuestions qQ1 = s.get(QuizQuestions.class, QQJD);
+                    s.delete(qQ1);
+                    List<QuizQuestions> qQL = quiz.getqQuestions();
+                    qQL.remove(qQ1);
+                    quiz.setqQuestions(qQL);
+                case 3:
+                    //Apply changes to the database and ends the loop
+                    s.update(quiz);
+                    s.getTransaction().commit();
+                    loop = false;
+            }
+        }
+    }
+
+    /**
+     * Added a method for deleting a quiz
+     * @param s - this hibernate session that is open for the duration of the program
+     */
+    public static void deleteQuiz(Session s){
+        Scanner sc = new Scanner(System.in);
+        s.beginTransaction();
+        Quiz quiz = selectQuiz(s, sc);
+        for (QuizQuestions qQ : quiz.getqQuestions()){//iterates through all the relationships between this
+            // quiz and questions and deletes them from the database accordingly
+            s.delete(qQ);
+        }
+        s.delete(quiz);
+        s.getTransaction().commit();
+    }
+    public static Quiz selectQuiz(Session s, Scanner sc){
+        System.out.println("Please enter the questionID of the quiz you wish to select");
+        while (!sc.hasNextInt()) { //ensures the questionID is an integer
+            System.out.println("Please enter an integer");
             sc.nextLine();
         }
         int quizID = sc.nextInt();
-
-    }
-    public static Quiz selectQuiz(Session s, Scanner sc){
-
+        sc.nextLine();
+        s.beginTransaction();
+        return s.get(Quiz.class, quizID);
     }
 }
